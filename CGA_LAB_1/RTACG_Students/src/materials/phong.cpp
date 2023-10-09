44577a233b2735852f0c7ae3f1fe2a4ca080a28c
Iker Landarech Mayo <iker.landarech01@estudiant.upf.edu>
2023-10-09 22:28:34 +0200

    FINAL FINAL CHANGES

--- CGA_LAB_1/RTACG_Students/src/materials/phong.cpp
+++ CGA_LAB_1/RTACG_Students/src/materials/phong.cpp
@@ -10,24 +10,14 @@ Phong::Phong(Vector3D kd_, Vector3D ks_, double alpha_) :
 {
 }
 
-//Vector3D Phong::getReflectance(const Vector3D& n, const Vector3D& wo, const Vector3D& wi) const {
-//
-//    Vector3D reflectance = Vector3D(0, 0, 0);
-//    Vector3D wr = ((2 * (n.x * wi.x) * n.x - wi.x), (2 * (n.y * wi.y) * n.y - wi.y), (2 * (n.z * wi.z) * n.z - wi.z));
-//    reflectance = ((kd.x * (wi.x * n.x) + ks.x * pow(wo.x * wr.x, alpha)), (kd.y * (wi.y * n.y) + ks.y * pow(wo.y * wr.y, alpha)), (kd.z * (wi.z * n.z) + ks.z * pow(wo.z * wr.z, alpha)));
-//
-//    return reflectance;
-//}
-
-Vector3D Phong::getReflectance(const Vector3D& n, const Vector3D& wo, const Vector3D& wi) const
-{
-	Vector3D r = kd * (dot(wi, n) < 0 ? 0 : dot(wi, n));
-	Vector3D calc_n = n;
-	calc_n.x *= (2 * (dot(n, wi) < 0 ? 0 : dot(n, wi)));
-	calc_n.y *= (2 * (dot(n, wi) < 0 ? 0 : dot(n, wi)));
-	calc_n.z *= (2 * (dot(n, wi) < 0 ? 0 : dot(n, wi)));
-	Vector3D wr = calc_n - wi;
-	return r + (ks * pow((dot(wo, wr) < 0 ? 0 : dot(wo, wr)), alpha));
+Vector3D Phong::getReflectance(const Vector3D& n, const Vector3D& wo, const Vector3D& wi) const {
+    double max = std::max(0.0, dot(n, wi));
+    Vector3D reflectance = { 0, 0, 0 };
+    Vector3D wr = ((n.operator*(dot(n, wi)) * 2.0) - wi).normalized();
+    reflectance.x = kd.x * max * dot(wi, n) + ks.x * max * pow(dot(wo, wr), alpha);
+    reflectance.y = kd.y * max * dot(wi, n) + ks.y * max * pow(dot(wo, wr), alpha);
+    reflectance.z = kd.z * max * dot(wi, n) + ks.z * max * pow(dot(wo, wr), alpha);
+    return reflectance;
 }
 
 bool Phong::hasSpecular() const
